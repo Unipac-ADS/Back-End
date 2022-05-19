379bef101ee557c3e710cd5b3caba0a4027a383c
vagasunipac <adsistemas2020@hotmail.com>
2022-05-19 19:19:31 -0300

    teste

--- src/test/java/br/com/stagiun/tccstagiun/model/repository/AlunoRepositoryTest.java
+++ src/test/java/br/com/stagiun/tccstagiun/model/repository/AlunoRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
--- src/test/java/br/com/stagiun/tccstagiun/resources/AlunoResourcesTes.java
+++ src/test/java/br/com/stagiun/tccstagiun/resources/AlunoResourcesTes.java
@@ -3,13 +3,14 @@ package br.com.stagiun.tccstagiun.resources;
 import br.com.stagiun.tccstagiun.controller.AlunoResources;
 import br.com.stagiun.tccstagiun.model.domain.Aluno;
 import br.com.stagiun.tccstagiun.model.service.AlunoService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -18,63 +19,113 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import util.JsonUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@Slf4j
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(AlunoResources.class)
-@Profile("Test")
+//@Profile("Test")
+@Slf4j
 public class AlunoResourcesTes {
 
-        @Autowired
-        private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private AlunoService service;
 
-        @MockBean
-        private AlunoService service;
-
-        private Aluno getAluno() {
-            return Aluno.builder()
-                    .nome("Brasil")
-                    .build();
-        }
-
-        @Test
-        public void find_by_countries_and_thenStatus200_and_aluno() throws Exception {
-            Long id = 1l;
-            String response = JsonUtil.getJson(getAluno());
-
-            Aluno aluno = getAluno();
-            given(service.findById(id).get()).willReturn(aluno);
-
-            mockMvc.perform(get("/api/v1/alunos/{id}", id)).andDo(print()).andExpect(status().isOk());
-            // .andExpect(content().string(response));
-        }
-
-        //@Test
-        public void givenCountries_whenGetCountries_thenStatus201() throws Exception {
-
-            Aluno url = getAluno();
-            when(this.service.salvar(url)).thenReturn(url);
-
-            RequestBuilder requestBuilder = MockMvcRequestBuilders
-                    .post("/api/v1/alunos")
-                    .accept(MediaType.APPLICATION_JSON).content(JsonUtil.getJson(getAluno()))
-                    .contentType(MediaType.APPLICATION_JSON);
-
-            MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-
-            MockHttpServletResponse response = result.getResponse();
-
-            String content = response.getContentAsString();
-            log.info(content);
-            assertEquals(HttpStatus.CREATED.value(), response.getStatus());
-        }
-
+    private Aluno getAluno() {
+        return Aluno.builder().nome("Brasil").build();
     }
+
+    private Aluno getAluno2() {
+        return Aluno.builder().nome("Antonio").build();
+    }
+
+    private Aluno getAluno3() {
+        return Aluno.builder().nome("Lua").build();
+    }
+
+    @Test
+    public void find_by_student_by_id_and_thenStatus200() throws Exception {
+        Long id = 1L;
+        String alunoJson = getAlunoJson();
+
+        Aluno aluno = getAluno();
+        when(service.findById(id)).thenReturn(Optional.ofNullable(aluno));
+        mockMvc.perform(get("/api/v1/alunos/{id}", id))
+                .andDo(print())
+                .andExpect(content().json(alunoJson))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void find_by_countries_and_thenStatus204() throws Exception {
+        Long id = 1L;
+        mockMvc.perform(get("/api/v1/alunos/{id}", id)).andDo(print()).andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void find_by_countries_and_thenStatus200_and_all_aluno() throws Exception {
+        List<Aluno> alunoList = new ArrayList<>();
+
+        Aluno aluno = getAluno();
+        alunoList.add(aluno);
+
+        Aluno aluno2 = getAluno2();
+        alunoList.add(aluno2);
+
+        Aluno aluno3 = getAluno3();
+        alunoList.add(aluno3);
+
+        when(service.list()).thenReturn(alunoList);
+        mockMvc.perform(get("/api/v1/alunos"))
+                .andDo(print())
+                .andExpect(content().json("[{\"nome\":\"Brasil\"},{\"nome\":\"Antonio\"},{\"nome\":\"Lua\"}]"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void givenCountries_whenSaveStudent_thenStatus201() throws Exception {
+        Aluno aluno = getAluno();
+        String alunoJson = getAlunoJson();
+
+        when(this.service.salvar(aluno)).thenReturn(aluno);
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/api/v1/alunos").accept(MediaType.APPLICATION_JSON).content(alunoJson).contentType(MediaType.APPLICATION_JSON);
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        MockHttpServletResponse response = result.getResponse();
+        String content = response.getContentAsString();
+        log.info(content);
+        assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+    }
+
+    @Test
+    public void givenCountries_whenUpdateStudent_thenStatus200() throws Exception {
+        Long id = 1L;
+        Aluno aluno = getAluno();
+        String alunoJson = getAlunoJson();
+
+        when(service.editar(id, aluno)).thenReturn(aluno);
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.put("/api/v1/alunos/{id}", id).accept(MediaType.APPLICATION_JSON).content(alunoJson).contentType(MediaType.APPLICATION_JSON);
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        MockHttpServletResponse response = result.getResponse();
+        String content = response.getContentAsString();
+        log.info(content);
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+    }
+
+    private String getAlunoJson() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(getAluno());
+    }
+
+}
+
